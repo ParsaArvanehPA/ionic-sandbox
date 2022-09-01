901b60ca56be8d4fcb853272447ef8fabedbbe63
parsa.arvaneh@gmail.com <parsa.arvaneh@gmail.com>
2022-09-01 18:45:16 +0430

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-sandbox',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
